fb7b23e6fa703d5d824c947aeb343d332cc899cc
iaarchiarora <98007567+iaarchiarora@users.noreply.github.com>
2024-10-14 15:01:18 +0530

    This is file which consist Primitive data types.

---
+++ PrimitveTypes.java
@@ -0,0 +1,17 @@
+class PrimitiveTypes{
+    public static void main(String[] args) {
+        
+        int intVal = 2147483647;
+        long longVal = 9223372036854775807L;
+        float floatVal = 3.14f;
+        double doubleVal = 3.14159265359;
+        char charVal = 'A';
+        boolean boolVal = true;
+       System.out.println("int Value: "+intVal);
+       System.out.println("long Value: "+longVal);
+       System.out.println("float Value: "+floatVal);
+       System.out.println("double Value: "+doubleVal);
+       System.out.println("char Value: "+charVal);
+       System.out.println("boolean Value: "+boolVal);
+    }
+}
